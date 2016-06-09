ada55d5803542a7caeb5010aa07bbdb66620218d
reychino-heisenberg <reychino@gmail.com>
2016-06-09 17:30:10 -0500

    Animation completed with a cubic structure

--- shg_animation.sh
+++ shg_animation.sh
@@ -28,15 +28,21 @@ echo "\documentclass[border=2mm]{standalone}
 \begin{tikzpicture}
     \begin{axis}[hide axis][]
     %%%% Auxiliar white lines to have same frame size: objects in white color
-    \addplot[domain=-7.6*pi:8.2*pi,mark=none, green, samples=2] {2.3};
-    \addplot[domain=-7.6*pi:8.2*pi,mark=none, green, samples=2] {-2};
+    \addplot[domain=-8*pi:9*pi,mark=none, white, samples=2] {2.3};
+    \addplot[domain=-8*pi:9*pi,mark=none, white, samples=2] {-2};
 
-    \addplot[domain=-0.5:0.5,mark=none, green, samples=2] {0};
+    %%%% STRUCTURE FOR INCIDENCE
+    % \addplot[thick,blue] graphics[xmin=-10,ymin=-1.7 ,xmax=10,ymax=0.3] {cube};
+    \addplot[thick,blue] graphics[xmin=-10,ymin=-1.7 ,xmax=10,ymax=0.3] {cube};
+
 
     %%%% INCOMING RED BEAM FROM -7*pi TO 0*pi
-    \addplot[domain=-8*pi:$step1*pi+pi,samples=200,red,thick,rotate around={-20:(0,0)}]{sin(deg(x))};
+    \addplot[domain=-8*pi:$step1*pi+pi,samples=200,red,thick,rotate around={-20:(0,0)}]{0.5*sin(deg(x/2))};
     \draw [red,thick,rotate around={-20:(0,0)},->] (-8*pi,0) -- ($step1*pi+pi,0);
     \addplot[domain=-6.5*pi:-6.4*pi,mark=none, white, samples=2] {2.3} node[below, black]{\small $\mathbf{E}(\omega)$} ;
+    
+
+
 \end{axis}
 \end{tikzpicture}
 
@@ -76,13 +82,16 @@ echo "\documentclass[border=2mm]{standalone}
 \begin{tikzpicture}
     \begin{axis}[hide axis][]
     %%%% Auxiliar white lines to have same frame size: objects in white color
-    \addplot[domain=-7.6*pi:8.2*pi,mark=none, green, samples=2] {2.3};
-    \addplot[domain=-7.6*pi:8.2*pi,mark=none, green, samples=2] {-2};
+    \addplot[domain=-8*pi:9*pi,mark=none, white, samples=2] {2.3};
+    \addplot[domain=-8*pi:9*pi,mark=none, white, samples=2] {-2};
 
-    \addplot[domain=-0.5:0.5,mark=none, green, samples=2] {0};
+    %%%% STRUCTURE FOR INCIDENCE
+    % \addplot[thick,blue] graphics[xmin=-10,ymin=-1.7 ,xmax=10,ymax=0.3] {cube};
+    \addplot[thick,blue] graphics[xmin=-10,ymin=-1.7 ,xmax=10,ymax=0.3] {cube};
+
 
     %%%% BANISHING BEAM FROM -7*pi TO 0*pi
-    \addplot[domain=-8*pi:0,samples=200,red,thick,rotate around={-20:(0,0)},opacity=$step1]{sin(deg(x))};
+    \addplot[domain=-8*pi:0,samples=200,red,thick,rotate around={-20:(0,0)},opacity=$step1]{0.5*sin(deg(x/2))};
     \draw [red,thick,rotate around={-20:(0,0)},->,opacity=$step1] (-8*pi,0) -- (0,0);
     \addplot[domain=-6.5*pi:-6.4*pi,mark=none, white, samples=2] {2.3} node[below,black,opacity=$step1]{\small $\mathbf{E}(\omega)$} ;
 
@@ -102,6 +111,7 @@ latex
 
 done
 
+parse=$i
 
 }
 
@@ -112,8 +122,9 @@ done
 
 outgoing_beam () {
 
-step1=1
-step2=0
+step1=0
+step2=0.01
+counter=0
 
 for (( i = $parse; i < $parse+$1; i++ )); do
 
@@ -130,23 +141,30 @@ echo "\documentclass[border=2mm]{standalone}
 \begin{tikzpicture}
     \begin{axis}[hide axis][]
     %%%% Auxiliar white lines to have same frame size: objects in white color
-    \addplot[domain=-7.6*pi:8.2*pi,mark=none, green, samples=2] {2.3};
-    \addplot[domain=-7.6*pi:8.2*pi,mark=none, green, samples=2] {-2};
+    \addplot[domain=-8*pi:9*pi,mark=none, white, samples=2] {2.3};
+    \addplot[domain=-8*pi:9*pi,mark=none, white, samples=2] {-2};
 
-    \addplot[domain=-0.5:0.5,mark=none, green, samples=2] {0};
+    %%%% STRUCTURE FOR INCIDENCE
+    % \addplot[thick,blue] graphics[xmin=-10,ymin=-1.7 ,xmax=10,ymax=0.3] {cube};
+    \addplot[thick,blue] graphics[xmin=-10,ymin=-1.7 ,xmax=10,ymax=0.3] {cube};
 
-    %%%% APPEARING ARROW AT 0,0
-    \addplot[domain=0:$step1*pi+pi,samples=200,blue,thick,rotate around={20:(0,0)}]{sin(deg(x))};
-    \draw [blue,thick,rotate around={20:(0,0)},->] (0,0) -- ($step1*pi+pi,0);
-    \addplot[domain=-0.1:0.1,mark=none, white, samples=2] {1.1} node[above,black,opacity=$step2]{\small $\mathbf{E}(2\omega)$} ;
+
+    %%%% OUTGOING BEAM FROM 0,0
+    \addplot[domain=0:$step2,samples=200,blue,thick,rotate around={20:(0,0)}]{0.5*sin(deg(x))};
+    \draw [blue,thick,rotate around={20:(0,0)},->] (0,0) -- ($step2,0);
+    \addplot[domain=-0.1:0.1,mark=none, white, samples=2] {1.2} node[above,black,opacity=1.5*$step1]{\small $\mathbf{E}(2\omega)$} ;
+
 \end{axis}
 \end{tikzpicture}
 
 
 \end{document}" >> shg-frame`printf "%03d\n" $i`.tex
 
-step1=`echo "$step1-1.5/$1" | bc -l` ### used to vanish a draw
-step2=`echo "$step2+1.5/$1" | bc -l` ### used to show a draw
+((counter++))
+
+step1=`echo "$step1+1.5/$1" | bc -l` ### used to show a draw
+step2=`echo "$step2+8*3.14159/$1" | bc -l` ### used to show a draw
+
 
 
 latex
@@ -159,10 +177,15 @@ done
 ################################################################################
 
 
+#### 70 frames
+# incoming_beam 28
+# wave_transition 14
+# outgoing_beam 28
 
-incoming_beam 1
-wave_transition 4
-outgoing_beam 4
+
+incoming_beam 12
+wave_transition 6
+outgoing_beam 12
 
 
-# 70 30 20
+
